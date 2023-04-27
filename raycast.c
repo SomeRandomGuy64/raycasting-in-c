b041c88df105877453ad82c7524fa677271c6904
somerandomguy64 <faizan163123@gmail.com>
2023-04-27 13:20:01 +0100

    player control

--- raycast.c
+++ raycast.c
@@ -3,16 +3,43 @@
 #include <GL/glut.h>
 #include <math.h>
 
+float px, py; //player position
+
+void drawPlayer() 
+{
+    glColor3f(1, 1, 0);
+    glPointSize(8);
+    glBegin(GL_POINTS);
+    glVertex2i(px, py);
+    glEnd();
+}
+
 void display()
 {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    drawPlayer();
     glutSwapBuffers();
+}
+
+void buttons(unsigned char key, int x, int y)
+{
+    int playerSpeed = 10;
+    if (key == 'a') { px -= playerSpeed; }
+    if (key == 'd') { px += playerSpeed; }
+    if (key == 'w') { py -= playerSpeed; }
+    if (key == 's') { py += playerSpeed; }
+
+    glutPostRedisplay();
 }
 
 void init()
 {
     glClearColor(0.3, 0.3, 0.3, 0);
     gluOrtho2D(0, 1024, 512, 0);
+
+    //initialise player position
+    px = 300;
+    py = 300;
 }
 
 int main(int argc, char* argv[])
@@ -23,5 +50,6 @@ int main(int argc, char* argv[])
     glutCreateWindow("Coding Cartel - Raycaster");
     init();
     glutDisplayFunc(display);
+    glutKeyboardFunc(buttons);
     glutMainLoop();
 }
