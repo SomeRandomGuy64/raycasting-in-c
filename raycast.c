e9a3229194b4ecc44be35640f22c40627813eaa4
somerandomguy64 <faizan163123@gmail.com>
2023-04-27 14:14:42 +0100

    draw rays to stop on horizontal lines

--- raycast.c
+++ raycast.c
@@ -63,11 +63,70 @@ void drawPlayer()
     glEnd();
 }
 
+void drawRays3D()
+{
+    int r, mx, my, mp, dof;
+    float rx, ry, ra, xo, yo;
+
+    ra = pa;
+
+    for (r = 0; r < 1; r++)
+    {
+        // Check for the horizontal lines
+        dof = 0;
+        float aTan =-1/tan(ra);
+        //looking up
+        if (ra > PI)
+        {
+            ry = (((int) py >> 6 ) << 6) - 0.0001; rx = (py - ry) * aTan + px; yo = - 64; xo = -yo * aTan;
+        }
+        //looking down
+        if (ra < PI)
+        {
+            ry = (((int) py >> 6 ) << 6) + 64; rx = (py - ry) * aTan + px; yo = 64; xo = -yo * aTan;
+        }
+        //looking straigh left or right
+        if (ra == 0 || ra == PI)
+        {
+            rx = px;
+            ry = py;
+            dof = 8;
+        }
+
+        while (dof < 8)
+        {
+            mx = (int) (rx) >> 6;
+            my = (int) (ry) >> 6;
+            mp = my * mapX + mx;
+
+            //hit wall
+            if (mp < mapX * mapY && map[mp] == 1)
+            {
+                dof = 8;
+            }
+            //next line
+            else
+            {
+                rx += xo;
+                ry += yo;
+                dof += 1;
+            }
+        }
+        glColor3f(0, 1, 0);
+        glLineWidth(1);
+        glBegin(GL_LINES);
+        glVertex2i(px, py);
+        glVertex2i(rx, ry);
+        glEnd();
+    }
+}
+
 void display()
 {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     drawMap2D();
     drawPlayer();
+    drawRays3D();
     glutSwapBuffers();
 }
 
